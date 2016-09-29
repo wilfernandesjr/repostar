273ad0cb6e55770c16908bb9f1c17710e02fd9cc
wilfernandesjr <wilmar@nanomedia.com.br>
2016-09-29 15:41:21 -0300

    fix bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/dist', express.static(__dirname + '/dist'));
+app.use('/repostar/dist', express.static(__dirname + '/dist'));
 
 app.all('*', function(req, res) {
   res
